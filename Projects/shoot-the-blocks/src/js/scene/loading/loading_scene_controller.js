87856fcc36bc5dcf475ae4fad33adb4c80fa3a44
Muhammad Yoga Nugraha Putra <m.yoga@agate.id>
2019-09-23 09:21:14 +0700

    addling loading screen

--- Projects/shoot-the-blocks/src/js/scene/loading/loading_scene_controller.js
+++ Projects/shoot-the-blocks/src/js/scene/loading/loading_scene_controller.js
@@ -24,12 +24,18 @@ export default class LoadingSceneController extends Phaser.Scene{
         this.view = new LoadingSceneView(this);
         this.view.create();
 
+        this.load.once('complete', this.LoadBootResoucesComplete);
+        
+        this.LoadBootResouces();
+    }
+
+    LoadBootResoucesComplete = () =>{
+        this.view.InitLoading();
         this.load.on('progress', function (value) {
             this.view.SetProgressText(value);
         },this);
         this.load.once('complete', this.OnCompleteLoad);  
-        
-        this.view.InitLoading();
+
         this.LoadResouces();
     }
 
@@ -47,6 +53,14 @@ export default class LoadingSceneController extends Phaser.Scene{
 
     }
     
+    LoadBootResouces(){
+        this.load.image('bg_loading',this.CreatePath('/images/loading/Background.png'));
+        this.load.image('loading_character',this.CreatePath('/images/loading/Character.png'));
+        this.load.image('loading_emptybar',this.CreatePath('/images/loading/Loading-Bar-Empty.png'));
+        this.load.image('loading_fullbar',this.CreatePath('/images/loading/Loading-Bar-Full.png'));
+
+        this.load.start();
+    }
 
     LoadResouces(){
         this.VoucherLoader.loadResource();
@@ -54,11 +68,8 @@ export default class LoadingSceneController extends Phaser.Scene{
         this.GameplayLoader.loadResource();
 
         this.load.image('logo',this.CreatePath('/images/Logo-BL.png'));
-        this.load.image('bg_loading',this.CreatePath('/images/loading/Background.png'));
-        this.load.image('loading_character',this.CreatePath('/images/loading/Character.png'));
-        this.load.image('loading_emptybar',this.CreatePath('/images/loading/Loading-Bar-Empty.png'));
-        this.load.image('loading_fullbar',this.CreatePath('/images/loading/Loading-Bar-Full.png'));
-
+       
+        this.load.start();
     }
 
     CreatePath(path){

2780bf14196979457da4089653c139adbf66c36b
Nicko Pratama <pratamareksas@gmail.com>
2019-10-01 19:35:56 +0700

    init project

---
+++ Projects/sort-the-box/src/js/module/objects/button.js
@@ -0,0 +1,117 @@
+import Image from './image';
+
+export default class Button extends Phaser.GameObjects.Container{
+
+    /** 
+    * @param {Phaser.Scene} scene 
+    * @param {Number} x
+    * @param {Number} y
+    * @param {String} texture
+    */
+    constructor(scene, x, y, texture){
+        super(scene, x, y);
+        
+        this.scene = scene;
+        /** @type {ScreenUtility}  */
+        this.ScreenUtility = scene.ScreenUtility;
+
+        this.IsEnabled = true;
+        this.IsAudioActive = true;
+
+        this.NormalTexture = texture;
+        this.HighlightTexture = null;
+        this.DisabledTexture = null;
+
+        this.Image = new Image(this.scene, 0, 0, this.NormalTexture);
+        this.Image.setInteractive();
+        this.add(this.Image);
+
+        this.IsClicked = false;
+
+        this.Image.on('pointerdown', this.pointerDown, this);
+        this.Image.on('pointerout', this.pointerOut, this);
+        
+        this.scene.add.existing(this);
+
+        this.EventList = {
+            onClick: "onClick",
+			onPointerDown: "onPointerDown",
+			onPointerUp: "onPointerUp",
+		}
+    }
+
+    pointerDown = (pointer) =>{
+        if(!this.IsEnabled)
+            return;
+
+        if(this.HighlightTexture != null){
+            this.Image.texture = this.HighlightTexture;
+        }
+        
+        this.IsClicked = true;
+        this.emit(this.EventList.onPointerDown);
+    }
+
+    pointerOut = (pointer) =>{
+        if(!this.IsEnabled)
+            return;
+
+        if(this.HighlightTexture != null){
+            this.Image.texture = this.NormalTexture;
+        }
+
+        this.emit(this.EventList.onPointerUp);
+        
+        if(this.IsClicked){
+            this.IsClicked = false;
+
+            if(this.IsAudioActive){
+                //play audio button
+                //this.scene.game.sound.play('click');
+            }
+
+            this.emit(this.EventList.onClick);
+        }
+    }
+
+    setOrigin(x, y = x){
+        this.Image.setOrigin(x, y);
+    }
+
+    setDisplayWidth(width, matchHeightToAspectRatio = false){
+        this.Image.setDisplayWidth(width, matchHeightToAspectRatio);
+    }
+
+    setDisplayHeight(height, matchWidthToAspectRatio = false){
+        this.Image.setDisplayHeight(height, matchWidthToAspectRatio);
+    }
+
+    setAudioActive(isActive = true){
+        this.IsAudioActive = isActive;
+    }
+
+    setEnable(active){
+        this.IsEnabled = active;
+        this.Image.texture = (this.DisabledTexture != null && !active) ? this.DisabledTexture : this.NormalTexture;
+    }
+
+    removeOnCLickListener(){
+        this.removeListener(this.EventList.onClick);
+    }
+
+    onClick(event){
+        this.on(this.EventList.onClick, event);
+    }
+
+    onceClick(event){
+        this.once(this.EventList.onClick, event);
+    }
+
+    onPointerDown(event){
+        this.on(this.EventList.onPointerDown, event, this);
+    }
+
+    onPointerUp(event){
+        this.on(this.EventList.onPointerUp, event, this);
+    }
+}
---
+++ Projects/sort-the-box/src/js/scene/gameplay/gameplay_scene_controller.js
@@ -0,0 +1,105 @@
+import GameplaySceneView from './gameplay_scene_view';
+import ScreenUtility from '../../module/screen/screen_utility';
+import VoucherView from '../../view/popup_voucher_view';
+import VoucherData from '../../voucherdata';
+
+export default class GameplaySceneController extends Phaser.Scene {
+	constructor() {
+        super({key: 'GameScene'});
+        
+    }
+
+    init = ()=>{
+        console.log('game screen')
+
+        this.initGame();
+        this.initGameData();
+        this.initAudio();
+    }
+
+    initGame = ()=>{
+        ScreenUtility.ResetGameScreen();
+        this.ScreenUtility = ScreenUtility.getInstance();
+    }
+
+    initGameData = ()=>{
+        this.IsGameStarted = false;
+        this.IsGameWin = true;
+    }
+
+    initAudio = ()=>{
+
+    }
+
+    create = ()=>{
+        this.view = new GameplaySceneView(this).create();
+
+        this.startGame();
+    }
+
+    startGame = ()=>{
+        this.IsGameStarted = true;
+
+        this.gameOver();
+    }
+
+    update(timestep, delta){
+        if(this.IsGameStarted){
+            this.gameUpdate(timestep, delta);
+        }
+
+    }
+
+    gameUpdate(timestep, delta){
+
+    }
+
+    gameOver = ()=>{
+        this.IsGameStarted = false;
+
+        this.showResult();
+    }
+
+    restart = ()=>{
+        this.scene.restart();
+    }
+
+    backToTitle = ()=>{
+        this.scene.launch('TitleScene');
+        this.scene.stop();
+    }
+
+    showResult = ()=>{
+        this.VoucherView = new VoucherView(this);
+        this.VoucherView.OnClickMainLagi(this.restart);
+        this.VoucherView.OnClickClose(this.backToTitle);
+        
+        let voucherData = VoucherData.Vouchers[CONFIG.VOUCHER_TYPE];
+
+        if(this.IsGameWin){
+            this.VoucherView.ShowVoucherCode(voucherData.Code, {
+                titleInfo :  voucherData.InfoTitle,
+                description : voucherData.InfoDescription,
+                expireDate : voucherData.ExpireDate,
+                minTransactionInfo : voucherData.MinimalTransactionInfo,
+                onlyAppliesInfo : voucherData.OnlyAppliesInfo,
+                termsandconditions : voucherData.TermsAndConditions,
+            });
+
+            this.VoucherView.SetDescription('voucher_headerwin', 
+                "Voucher", 
+                voucherData.Title, 
+                voucherData.Description
+            );
+        }else{
+            this.VoucherView.DisableVoucherCode()
+            this.VoucherView.SetDescription('voucher_headertimeout', 
+                "Timeout", 
+                VoucherData.VoucherTimeout.Title, 
+                VoucherData.VoucherTimeout.Description
+            );
+        }
+        
+        this.VoucherView.Open();
+    }
+}
